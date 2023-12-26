74719dd5a1c490c39d54fa9be2d5580cbd57ab97
Shadrack Ani <nnaemeka@zestpayment.com>
2023-12-26 08:45:14 +0100

    fixed bug with image uplaod

--- src/app/crime-form/crime-form.component.ts
+++ src/app/crime-form/crime-form.component.ts
@@ -10,54 +10,69 @@ export class CrimeFormComponent implements OnInit {
   uploadNumbers: any[] = ['image1', 'image2', 'image3', 'image4'];
   images: any[] = [];
   imageFile: File[] = [];
-  constructor( private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   reportDetails: FormGroup = this.fb.group({
     time: new FormControl(null, [Validators.required]),
     date: new FormControl('', Validators.required),
     product_file: new FormControl(this.images, Validators.required),
-   crimeType: new FormControl('', [
-      Validators.required,
-      
-    ]),
+    crimeType: new FormControl('', [Validators.required]),
     fullName: new FormControl(null, [Validators.required]),
     socials: new FormControl('', Validators.required),
     handle: new FormControl('', Validators.required),
-    email : new FormControl('', [Validators.required,  Validators.pattern(
-            '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$'
-          ),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$'),
     ]),
-    phoneNumber : new FormControl ('', Validators.required),
-    description : new FormControl ('', Validators.required),
-  
-     file: new FormControl(this.images, Validators.required)
- 
+    phoneNumber: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+
+    file: new FormControl(this.images, Validators.required),
   });
 
   uploadFile($event: Event, index: number) {
     const target = $event.target as HTMLInputElement;
-    let files: any = target.files;
+    const files: any = target.files;
+
     this.imageFile[index] = files[0];
 
+    console.log('Current images length:', this.images.length);
+
     if (this.images.length >= 4) {
-      alert('Not more than 3 uploads');
+      alert('Not more than 4 uploads');
       return;
     } else {
-      const reader = new FileReader();
-      reader.addEventListener('load', () => {
-        const uploaded_image = reader.result;
-        this.images[index] = uploaded_image;
+      const processFile = (fileIndex: number): Promise<void> => {
+        return new Promise((resolve) => {
+          const reader = new FileReader();
+          reader.addEventListener('load', () => {
+            const uploaded_image = reader.result;
+            // Append to the array instead of replacing
+            this.images.push(uploaded_image);
+
+            console.log('Updated images length:', this.images.length);
+
+            resolve();
+          });
+
+          reader.readAsDataURL(files[fileIndex]);
+        });
+      };
+
+      const processFilesSequentially = async () => {
+        for (let i = 0; i < files.length; i++) {
+          await processFile(i);
+        }
+
         this.reportDetails.patchValue({
           files: this.images,
         });
-        return this.images;
-      });
 
-      for (let i = 0; i < files.length; i++) {
-        reader.readAsDataURL(files[i]);
-      }
+        return this.images;
+      };
+
+      processFilesSequentially();
     }
   }
 
